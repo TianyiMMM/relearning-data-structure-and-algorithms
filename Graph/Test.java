6be2719da16c0b22b5d0c3c842ca0bf7b1045e3c
Tianyi Ma <tianyi.ma01@gmail.com>
2020-11-30 19:36:32 +0800

    Add files via upload

--- Graph/Test.java
+++ Graph/Test.java
@@ -1,4 +1,48 @@
+import java.util.*;
+
 public class Test{
+	public static void DFS(Node node){
+		System.out.println("Node "+node.data);
+		node.marked = true;
+		for (int i = 0; i < node.adjacent_nodes.length; i++){
+			Node adj_node = node.adjacent_nodes[i];
+			if (adj_node!=null){
+				if (adj_node.marked == false){
+					DFS(adj_node);
+				}
+			}
+		}
+	}
+
+	public static void BFS(Node root){
+		Queue<Node> q = new LinkedList<>();
+		root.marked = true;
+		q.add(root);
+
+		while(!q.isEmpty()){
+			Node node = q.remove();
+			System.out.println("Node "+node.data);
+			for(int i = 0; i<node.adjacent_nodes.length; i++){
+				Node adj_node = node.adjacent_nodes[i];
+				if (adj_node!=null){
+					if (adj_node.marked==false){
+						adj_node.marked = true;
+						q.add(adj_node);
+					}
+				}
+			}
+		}
+	}
+
+	public static void resetGraphSearch(Graph g){
+		Node[] nodes = g.nodes;
+		for (int i = 0; i < nodes.length; i++){
+			if (nodes[i]!=null){
+				nodes[i].marked = false;
+			}
+		}
+	}
+
 	public static void main(String[] arg){
 		// a graph with nodes with 1 < data <= n, nodes' adjacent nodes list length <= 10
 		// example from page.107 of Crack the Technical Interview
@@ -20,6 +64,12 @@ public class Test{
 		g.addEdge(n2, n1);
 		g.addEdge(n3, n2);
 		g.addEdge(n3, n4);
-		g.print();
+		//g.print();
+		System.out.println("DFS");
+		DFS(n0);
+		System.out.println();
+		resetGraphSearch(g);
+		System.out.println("BFS");
+		BFS(n0);
 	}
 }
